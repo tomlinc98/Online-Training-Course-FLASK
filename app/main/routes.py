853b62c6a43bc8df25ff9acd01a17c82d8a47c46
Chris <tomlinc98@gmail.com>
2022-11-11 16:16:37 +0000

    courses

--- app/main/routes.py
+++ app/main/routes.py
@@ -17,3 +17,10 @@ def courses(cat=None):
 def about(cat=None):
     return render_template('about.html',  title='About')
 
+"""@app.route("/details/<int:pet_id>")
+def pet_details(pet_id):
+    # View function for Showing Details of Each Pet. # 
+    pet = next((pet for pet in pets if pet["id"] == pet_id), None) 
+    if pet is None: 
+        abort(404, description="No Pet was Found with the given ID")
+    return render_template("details.html", pet = pet) """
